32706ddd1ac9945ae33ac7ab0be44e6e6cfb3296
Dylan Leonard <dleonard@csumb.edu>
2022-04-04 23:18:34 -0700

    Updated Tower Defense Project from Part 1 to Part 2

--- Tower Defense/Assets/Scripts/SpawnEnemy.cs
+++ Tower Defense/Assets/Scripts/SpawnEnemy.cs
@@ -7,7 +7,6 @@ public class SpawnEnemy : MonoBehaviour
     public GameObject enemy;
 
     public List<Transform> waypoints;
-
     public List<Material> materials;
     // Start is called before the first frame update
     void Start()
